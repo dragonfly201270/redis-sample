072302b477f28193fd5ecb9b3af3645c91b6cd1c
dragonfly201270 <adi.3183@gmail.com>
2017-02-23 14:48:32 +0530

    new commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.AspNetCore.Http;
+
+namespace WebApplication9.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+           
+        
+            HttpContext.Session.SetString("User", "Rami");
+            return Json(true);
+            
+        
+    }
+
+        public IActionResult About()
+        {
+        var userContent = HttpContext.Session.GetString("User");
+        return Json(userContent);
+    }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
